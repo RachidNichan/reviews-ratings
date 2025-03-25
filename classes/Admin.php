5f3665ec0fe8e3d1550c2752ecb2fd2a15cbd905
RachidNichan <rachidarn95@gmail.com>
2025-03-25 22:34:17 +0000

    Modified Admin.php file

--- classes/Admin.php
+++ classes/Admin.php
@@ -1,100 +1,158 @@
 <?php
 
 class Admin {
-    private $_db;
-    private $_data;
+    private $_db,
+            $_data;
 
     public function __construct() {
         $this->_db = DB::getInstance();
     }
 
     public function getThemes() {
-        return glob('views/themes/*', GLOB_ONLYDIR);
+        $themes = glob('views/themes/*', GLOB_ONLYDIR);
+        return $themes;
     }
 
-    public function addNewPage(array $fields) {
-        return $this->_db->insert('nr_custompages', $fields);
+    public function addNewPage($fields = array()) {
+        if ($this->_db->insert('nr_custompages', $fields)) {
+            return true;
+        }
     }
 
-    public function updateCustomPage(array $fields, $id) {
-        return $id ? $this->_db->update('nr_custompages', $id, $fields) : false;
+    public function updateCustomPage($fields = array(), $id = null) {
+        if ($this->_db->update('nr_custompages', $id, $fields)) {
+            return true;
+        }
     }
 
     public function custompagesData($start = 0, $end = 0) {
         $data = $this->_db->allLimit('nr_custompages', $start, $end);
-        return $data->count() ? $data->results() : [];
+
+        if ($data->count()) {
+            return $data->results();
+        }
     }
 
     public function updateAdsCode($key, $ads) {
-        return $this->_db->query("UPDATE `nr_ads` SET `code` = ? WHERE `type` = ?", [$ads, $key]);
+        $sql = "UPDATE `nr_ads` SET `code` = ? WHERE `type` = '{$key}'";
+        if ($this->_db->query($sql, array('code' => $ads))) {
+            return true;
+        }
     }
 
     public function updateTerms($key, $value) {
-        return $this->_db->query("UPDATE `nr_terms` SET `text` = ? WHERE `type` = ?", [$value, $key]);
+        $sql = "UPDATE `nr_terms` SET `text` = ? WHERE `type` = '{$key}'";
+        if ($this->_db->query($sql, array('text' => $value))) {
+            return true;
+        }
     }
 
-    public function deleteData($table, $id) {
-        return $table && $id ? $this->_db->delete($table, ['id', '=', $id]) : false;
+    public function deleteData($table = null, $id = 0) {
+        $this->_db->delete($table, array('id', '=', $id));
+        return true;
     }
 
-    public function fetchData($table, $start = 0, $end = 0) {
-        $data = $this->_db->allLimit($table, $start, $end);
-        return $data->count() ? $data->results() : [];
+    public function categoriesData($start = 0, $end = 0) {
+        $data = $this->_db->allLimit('nr_categories', $start, $end);
+
+        if ($data->count()) {
+            return $data->results();
+        }
+    }
+
+    public function categoriesMainData($start = 0, $end = 0) {
+        $data = $this->_db->allLimit('nr_categories_main', $start, $end);
+
+        if ($data->count()) {
+            return $data->results();
+        }
+    }
+    
+    public function postsData($start = 0, $end = 0) {
+        $data = $this->_db->allLimit('nr_posts', $start, $end);
+
+        if ($data->count()) {
+            return $data->results();
+        }
+    }
+
+    public function reviewData($start = 0, $end = 0) {
+        $data = $this->_db->allLimit('nr_review', $start, $end);
+
+        if ($data->count()) {
+            return $data->results();
+        }
     }
 
     public function usersData($start = 0, $end = 0) {
-        return $this->fetchData('nr_users', $start, $end);
+        $data = $this->_db->allLimit('nr_users', $start, $end);
+
+        if ($data->count()) {
+            return $data->results();
+        }
     }
 
     public function addNewLanguage($lang_name) {
-        $lang_name = preg_replace('/[^a-zA-Z0-9_]/', '', $lang_name); // Sanitize input
         $sql = "ALTER TABLE `nr_langs` ADD `$lang_name` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;";
-        
         if ($this->_db->query($sql)) {
-            $langs = $this->langsFromDB('english');
-            foreach ($langs as $key) {
-                $update_sql = "UPDATE `nr_langs` SET `$lang_name` = ? WHERE `lang_key` = ?";
-                $this->_db->query($update_sql, [$key->english, $key->lang_key]);
+            foreach (self::langsFromDB('english') as $key) {
+                $sql = "UPDATE `nr_langs` SET `{$lang_name}` = '{$key->english}' WHERE `lang_key` = '{$key->lang_key}'";
+                if ($this->_db->query($sql)) {
+                    return true;
+                }
             }
-            return true;
         }
-        return false;
     }
 
-    public function addNewKey(array $fields) {
-        return $this->_db->insert('nr_langs', $fields);
+    public function addNewKey($fields = array()) {
+        if ($this->_db->insert('nr_langs', $fields)) {
+            return true;
+        }
     }
 
     public function langsFromDB($lang = 'english') {
-        return $this->_db->query("SELECT `lang_key`, `$lang` FROM `nr_langs`")->results();
+        $sql = "SELECT `lang_key`, `$lang` FROM `nr_langs`";
+        if ($this->_db->query($sql)) {
+            return $this->_db->results();
+        }
     }
 
     public function updateConfig($key, $value) {
-        return $this->_db->query("UPDATE `nr_config` SET `value` = ? WHERE `name` = ?", [$value, $key]);
+        $sql = "UPDATE `nr_config` SET `value` = ? WHERE `name` = '{$key}'";
+        if ($this->_db->query($sql, array('value' => $value))) {
+            return true;
+        }
     }
 
-    public function countAllData($type) {
-        return $type ? number_format($this->_db->all($type)->count()) : 0;
+    public function countAllData($type = null) {
+        $data = $this->_db->all($type);
+        return number_format($data->count());
     }
 
     public function countOnlineUser() {
         $time = time() - 60;
-        return number_format($this->_db->get('nr_users', ['lastseen', '>', $time])->count());
+        $data = $this->_db->get('nr_users', array('lastseen', '>', $time));
+        return number_format($data->count());
     }
 
     public function usersDataOnline($start = 0, $end = 0) {
         $time = time() - 60;
-        $data = $this->_db->allWithLimit('nr_users', ['lastseen', '>', $time], $start, $end);
-        return $data->count() ? $data->results() : [];
+        $data = $this->_db->allWithLimit('nr_users', array('lastseen', '>', $time), $start, $end);
+
+        if ($data->count()) {
+            return $data->results();
+        }
     }
 
-    public function registeredData($month, $type) {
+    public function registeredData($month, $type = null) {
         $year = date("Y");
-        return $this->_db->get($type, ['registered', '=', "$month/$year"])->count();
+        $data = $this->_db->get($type, array('registered', '=', $month.'/'.$year));
+        return $data->count();
     }
 
     public function data() {
         return $this->_data;
     }
 }
+
 ?>
